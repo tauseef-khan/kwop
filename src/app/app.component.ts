5238f318240714148b1446c03359ea0b8c83ae7c
tauseef-khan <tdakhan@gmail.com>
2020-05-09 01:32:16 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'kw-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'kwop';
+}
