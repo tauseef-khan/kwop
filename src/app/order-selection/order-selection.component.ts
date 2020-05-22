64d0920bc869c4db15359b7a75be8fbed05698a7
tauseef-khan <tdakhan@gmail.com>
2020-05-22 05:24:04 +0100

    utilised the item sevice in the order selection component

--- src/app/order-selection/order-selection.component.ts
+++ src/app/order-selection/order-selection.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { IItem } from '../item';
+import { ItemService } from '../item.service';
 
 @Component({
   selector: 'kw-order-selection',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./order-selection.component.css']
 })
 export class OrderSelectionComponent implements OnInit {
+  items: IItem[] = [];
+  errorMessage: string;
 
-  constructor() { }
+  constructor(private itemService: ItemService) { }
 
   ngOnInit(): void {
+    this.itemService.getItems().subscribe({
+      next: items => {
+        this.items = items
+      },
+      error: err => this.errorMessage = err
+    })
   }
 
 }
