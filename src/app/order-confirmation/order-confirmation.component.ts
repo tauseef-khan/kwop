907733b36c4dfe1552ea14d782e9cb6c5c5c454a
tauseef-khan <tdakhan@gmail.com>
2020-08-23 13:15:58 +0100

    final edits to complete the qr code feature

--- src/app/order-confirmation/order-confirmation.component.ts
+++ src/app/order-confirmation/order-confirmation.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IItem } from '../item';
 
 @Component({
   selector: 'kw-order-confirmation',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrderConfirmationComponent implements OnInit {
 
+  orderedItems: IItem[] = [];
+  orderedItemNames: string = "";
+
   constructor() { }
 
   ngOnInit(): void {
+    this.orderedItems = history.state.data;
+    this.orderedItemNames = this.getItemNames(this.orderedItems);
+  }
+
+  getItemNames(items: IItem[]): string {
+    for (let item of items) {
+      this.orderedItemNames += '- ' + item.itemName + '\n';
+    }
+
+    return this.orderedItemNames;
   }
 
 }
